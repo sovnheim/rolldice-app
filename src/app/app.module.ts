825a64c36d0df34ffaec592ec11c3b7ed2860078
root <felix.machiavelli@gmail.com>
2020-11-05 15:30:35 -0800

    made minor updates to dice input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { DiceBoxComponent } from './dice-box/dice-box.component';
 
@@ -20,6 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
