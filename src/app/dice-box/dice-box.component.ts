8ed34bd56fdb72b92042c557f28cea729d6a26de
root <felix.machiavelli@gmail.com>
2020-11-12 16:30:51 -0800

    bullshit regex

--- src/app/dice-box/dice-box.component.ts
+++ src/app/dice-box/dice-box.component.ts
@@ -5,11 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dice-box.component.html',
   styleUrls: ['./dice-box.component.css']
 })
+
 export class DiceBoxComponent implements OnInit {
 
   diceCommand = ''; // There's probably something better to do here than initialize an empty variable
+  validCommand = false;
+
+  // re = new RegExp('(?i)^(?P<dCount>\d+)d(?P<dType>\d+) ?(?P<dMod>[+-]\d+)?$');
+
+  const regex = /^coucou/gm;
+  const chaine = 'coucou le monde !';
+
+  const resultat = regex.test(chaine);
+
+  console;.log(resultat); // true
 
   constructor() { }
+
+  onKey(event: any): void { // without type info
+    this.diceCommand = event.target.value;
+    console.log('fuck all this and that');
+  }
 
   ngOnInit(): void {
   }
