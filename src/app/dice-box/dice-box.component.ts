a18d3f8e6ed38f4620dfb48fe36f8eb9d04cc848
root <felix.machiavelli@gmail.com>
2020-11-19 15:59:33 -0800

    mvp up and running

--- src/app/dice-box/dice-box.component.ts
+++ src/app/dice-box/dice-box.component.ts
@@ -1,7 +1,7 @@
-import { ArrayType } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
-const VALIDINPUT = /^(?<dCount>\d+)[Dd](?<dType>\d+) ?(?<dMod>\+\d+)?$/gm;
+const DICE_COMMAND_REGEX = /^(?<dCount>\d+)[Dd](?<dType>\d+) ?(?<dMod>\+\d+)?$/;
 
 @Component({
   selector: 'app-dice-box',
@@ -12,6 +12,7 @@ const VALIDINPUT = /^(?<dCount>\d+)[Dd](?<dType>\d+) ?(?<dMod>\+\d+)?$/gm;
 export class DiceBoxComponent implements OnInit {
 
   d: DiceModel;
+  seconds: number; // rxjs
 
   constructor() {
     this.d = new DiceModel();
@@ -19,11 +20,39 @@ export class DiceBoxComponent implements OnInit {
 
   onKey(event: any): void {
     if (/^\w$/gm.test(event.key)) {
-      this.d.validCommand = VALIDINPUT.test(this.d.diceCommand);
+      this.d.validCommand = DICE_COMMAND_REGEX.test(this.d.diceCommand);
+      if (this.d.validCommand) {
+        this.d.rollableSet = this.parseDiceCommand(this.d.diceCommand);
+        this.d.rolledSet = this.getDiceResults(this.d.rollableSet);
+        this.d.updateResult();
+      }
     }
   }
 
   ngOnInit(): void {
+    const counter = interval(1000); // rxjs
+    counter.subscribe(
+      value => { this.seconds = value; }
+    );
+  }
+
+  parseDiceCommand(diceCommand: string): number[] {
+    const parsedSet = DICE_COMMAND_REGEX.exec(diceCommand);
+    const rollableSet: number[] = [];
+
+    for (let i = 0; i < Number(parsedSet.groups.dCount); i++) {
+      rollableSet.push(Number(parsedSet.groups.dType));
+    }
+    return rollableSet;
+  }
+
+  getDiceResults(rollable: number[]): number[] {
+    const result = [];
+
+    for (const die of rollable) {
+      result.push(Math.floor(Math.random() * die) + 1);
+    }
+    return result;
   }
 }
 
@@ -31,10 +60,20 @@ export class DiceModel {
 
   diceCommand: string;
   validCommand: boolean;
+  rollableSet: number[];
+  rolledSet: number[];
+  modifiers: number[];
+  result: number;
 
-constructor(){
-    this.diceCommand = '';
-    this.validCommand = false;
+  constructor(){
+      this.diceCommand = '';
+      this.validCommand = false;
+    }
+
+  updateResult(): void {
+    this.result = this.rolledSet.reduce((a, b) => a + b, 0);
+    if (this.modifiers){
+      this.result += this.modifiers.reduce((a, b) => a + b, 0);
+    }
   }
-
 }
